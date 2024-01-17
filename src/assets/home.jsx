d8bc410c0225ccac4b78bfda01554f20c725311a
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-01-17 19:09:28 +0630

    add pagination

--- src/assets/home.jsx
+++ src/assets/home.jsx
@@ -1,56 +1,90 @@
-import BurmeseRecipes from "../../BurmeseRecipes.json"
+import React, { useState } from 'react';
+import BurmeseRecipes from "../../BurmeseRecipes.json";
+
+const itemsPerPage = 8; 
 
 export default function Home() {
-    console.log(BurmeseRecipes)
-    return (
-        <div className="container mx-auto  bg-slate-500">
-            <section className="bg-white py-8">
+    const [currentPage, setCurrentPage] = useState(1);
 
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+    const currentItems = BurmeseRecipes.slice(indexOfFirstItem, indexOfLastItem);
+    const totalPages = Math.ceil(BurmeseRecipes.length / itemsPerPage);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage(currentPage - 1);
+    };
+
+    const handleNextPage = () => {
+        setCurrentPage(currentPage + 1);
+    };
+
+    return (
+        <div className="container mx-auto bg-slate-500">
+            <section className="bg-white py-8">
                 <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
 
                     <nav id="store" className="w-full z-30 top-0 px-6 py-1">
                         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-
                             <a className="uppercase tracking-wide title no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
                                 Burmese Recipes
                             </a>
+                            <div className="flex justify-center">
+                                <button
+                                    className={`mx-2 py-2 px-4 rounded ${currentPage === 1 ? 'bg-gray-300 text-gray-800' : 'bg-gray-800 text-white'}`}
+                                    onClick={handlePrevPage}
+                                    disabled={currentPage === 1}
+                                >
+                                    &lt; 
+                                </button>
 
-                            {/* <div className="flex items-center" id="store-nav-content">
+                                {Array.from({ length: totalPages }, (_, index) => (
+                                    <button
+                                        key={index}
+                                        className={`mx-2 py-2 px-4 rounded ${currentPage === index + 1 ? 'bg-gray-800 text-white' : 'bg-gray-300 text-gray-800'}`}
+                                        onClick={() => handlePageChange(index + 1)}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                ))}
 
-                                <a className="pl-3 inline-block no-underline hover:text-black" href="#">
-                                    <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                        <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
-                                    </svg>
-                                </a>
-
-                                <a className="pl-3 inline-block no-underline hover:text-black" href="#">
-                                    <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                        <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
-                                    </svg>
-                                </a>
-
-                            </div> */}
+                                <button
+                                    className={`mx-2 py-2 d-flex  px-4 rounded ${currentPage === totalPages ? 'bg-gray-300 text-gray-800' : 'bg-gray-800 text-white'}`}
+                                    onClick={handleNextPage}
+                                    disabled={currentPage === totalPages}
+                                >
+                                     &gt;
+                                </button>
+                            </div>
                         </div>
                     </nav>
-                    {BurmeseRecipes.map((item,index) =>
-                        <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col my-16 " key={index}>
-                            <a href="#">
-                                <img
-                                    className="hover:grow hover:shadow-lg"
-                                    src={`img/${item.Name}.jpg`}
-                                    onError={(e) => {
-                                        e.target.src = 'img/default.png'; 
-                                    }}
-                                    style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-                                />
-                                <div className="pt-3 flex items-center justify-between">
-                                    <p className="">{item.Name}</p>
-                                </div>
-                            </a>
-                        </div> 
-                    )}              
+
+                    <div className="container mx-auto flex items-center flex-wrap pb-12">
+                        {currentItems.map((item, index) => (
+                            <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col my-16" key={index}>
+                                <a href="#">
+                                    <img
+                                        className="hover:grow hover:shadow-xl rounded-lg"
+                                        src={`img/${item.Name}.jpg`}
+                                        onError={(e) => {
+                                            e.target.src = 'img/default.png';
+                                        }}
+                                        style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                                    />
+                                    <div className="pt-3 flex items-center justify-between">
+                                        <p className="">{item.Name}</p>
+                                    </div>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
         </div>
-    )
+    );
 }
